7498a0821b0c3b54888d201a625f160b65259907
nNoidea <58337790+nNoidea@users.noreply.github.com>
2023-10-14 23:44:20 +0200

    removed the await to skip the loading screen.

--- src/modules/preload.tsx
+++ src/modules/preload.tsx
@@ -31,7 +31,7 @@ export async function preload(GLOBALS: Globals) {
 
     let cloud = await misSessionLogin();
     if (cloud != false) {
-        await CloudDBHandler(cloud);
+        CloudDBHandler(cloud);
     }
     // Everything is loaded
     setPreloaded(true);
