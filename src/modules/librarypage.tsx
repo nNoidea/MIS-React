0579d5fdf4326ecb75a521705d07719c53c7d298
nNoidea <58337790+nNoidea@users.noreply.github.com>
2023-10-16 22:22:03 +0200

    Separeted TV shows and movies in library. Movies will be sorted by unwatched.

--- src/modules/librarypage.tsx
+++ src/modules/librarypage.tsx
@@ -1,18 +1,31 @@
+import { Movie, TV } from "../classes/Media";
 import { Globals } from "../interfaces/interfaces";
 import { DBGetAll, objectStoreNameLibrary } from "./indexedDB";
 import { GridItems } from "./searchResults";
 
 export async function setupLibraryPage(GLOBALS: Globals) {
     const { setContent } = GLOBALS.SETTERS;
-    const movieArray = (await DBGetAll(objectStoreNameLibrary)).filter((media) => media.inLibrary);
+    const mediaArray = (await DBGetAll(objectStoreNameLibrary)).filter((media) => media.inLibrary);
+    const movieArray = mediaArray.filter((media): media is Movie => media instanceof Movie);
+    const movieArrayWatched = movieArray.filter((media) => media.watched);
+    const movieArrayUnwatched = movieArray.filter((media) => !media.watched);
+    const tvArray = mediaArray.filter((media) => media instanceof TV);
 
     setContent(
         <>
+            <h1 style={{ paddingLeft: "100px", paddingTop: "50px", color: "white" }}>Movies</h1>
             <div
                 className="grid-container"
                 id="searchResults"
             >
-                {GridItems(GLOBALS, movieArray)}
+                {GridItems(GLOBALS, movieArrayUnwatched.concat(movieArrayWatched))}
+            </div>
+            <h1 style={{ paddingLeft: "100px", paddingTop: "50px", color: "white" }}>TV Shows</h1>
+            <div
+                className="grid-container"
+                id="searchResults"
+            >
+                {GridItems(GLOBALS, tvArray)}
             </div>
         </>
     );
