4fbab40392fd8fbd5a0c691c97f6e84ee6a44dd9
nNoidea <oguz-nrb@hotmail.com>
2024-11-10 11:34:47 +0100

    test

--- src/modules/LoginModal.tsx
+++ src/modules/LoginModal.tsx
@@ -134,7 +134,7 @@ export function LoginModal(GLOBALS: Globals) {
             return (
                 <>
                     <Form id="Login">
-                        <h1>Loginaa</h1>
+                        <h1>Login HEREE</h1>
                         <Form.Group className="mb-3">
                             <Form.Label>Email address</Form.Label>
                             <Form.Control
